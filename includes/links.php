c8afdaa29489cfeaa52e26168f477173fb81950a
Eric <webmaster@coldcandor.com>
2010-06-19 16:18:47 -0400

    Initial commit of files from www

---
+++ includes/links.php
@@ -0,0 +1,31 @@
+			
+		<nav id="main_nav">
+			<ul>
+				<li><a href="index.php">Home</a></li>
+				<li><a href="webpages.php">Websites</a></li>
+				<li><a href="christmas.php">XMas List</a></li>
+				<li><a href="todo.php">To Do List</a></li>
+			</ul>
+		</nav>
+			
+		<nav id="links">
+			<ul>
+				<li><a href="http://mail.yahoo.com">Yahoo Mail</a></li>
+				<li><a href="http://mail.yahoo.com">Yahoo Mail</a></li>
+				<li><a href="http://mail.yahoo.com">Yahoo Mail</a></li>
+				<li><a href="http://mail.yahoo.com">Yahoo Mail</a></li>
+				<li><a href="http://mail.yahoo.com">Yahoo Mail</a></li>
+				<li><a href="http://mail.yahoo.com">Yahoo Mail</a></li>
+			</ul>
+		</nav>
+		
+		<nav id="foot_nav">
+			<ul>
+				<li><a href="mailto:webmaster@coldcandor.com">Contact Me</a></li>
+				<li><a href="mailto:webmaster@coldcandor.com">Contact Me</a></li>
+				<li><a href="mailto:webmaster@coldcandor.com">Contact Me</a></li>
+				<li><a href="mailto:webmaster@coldcandor.com">Contact Me</a></li>
+				<li><a href="mailto:webmaster@coldcandor.com">Contact Me</a></li>
+				<li><a href="mailto:webmaster@coldcandor.com">Contact Me</a></li>
+			</ul>
+		</nav>
